ebee60127a1b6293ed2b78cbbb070ad93e7719df
Howard <howardtuan@gmail.com>
2022-12-17 22:30:02 +0800

    basic done

--- myapp/views.py
+++ myapp/views.py
@@ -19,3 +19,30 @@ def index_view(request):
         'user_point':user_point,
         'user_barcode':user_barcode,
     } )
+def apps_view(request):
+    return render(request, 'orthers_app.html', {
+    } )
+def exchange_view(request):
+    return render(request, 'exchange.html', {
+    } )
+def fix_view(request):
+    return render(request, 'fix.html', {
+    } )
+def history_view(request):
+    return render(request, 'history.html', {
+    } )
+def login_view(request):
+    return render(request, 'login.html', {
+    } )
+def member_view(request):
+    return render(request, 'member.html', {
+    } )
+def myself_view(request):
+    return render(request, 'myself.html', {
+    } )
+def question_view(request):
+    return render(request, 'question.html', {
+    } )
+def signup_view(request):
+    return render(request, 'signup.html', {
+    } )
