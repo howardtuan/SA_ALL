663df766abc7bec566beda5bd0b31895356917ab
Howard <howardtuan@gmail.com>
2023-01-03 19:42:40 +0800

    o to i

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,5 @@ from news.views import HISTORYViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'history', HISTORYViewSet, basename='hostory')
+router.register(r'history', HISTORYViewSet, basename='history')
 urlpatterns = router.urls
